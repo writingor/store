cd11c62e447c650231bd9b8ef0cd34a154061b0f
Igor Rasskazov <writingornet@gmail.com>
2022-01-25 06:16:16 -0800

    Create Book class

---
+++ domain/Store/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace Store;
+public class Book
+{
+    public int Id { get; }
+    public string Title { get; }
+    //public string Author { get; }
+
+    public Book(int id, string title)
+    {
+        Id = id;
+        Title = title;
+    }
+}
