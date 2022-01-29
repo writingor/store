36a74d16510dc8a5d49a7abdba47cc78ac56f775
Igor Rasskazov <writingornet@gmail.com>
2022-01-29 07:47:04 -0800

    add search page

--- presentation/Store.Web/Program.cs
+++ presentation/Store.Web/Program.cs
@@ -1,7 +1,11 @@
+using Store;
+using Store.Memory;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<IBookRepository, BookRepository>();
 
 var app = builder.Build();
 
