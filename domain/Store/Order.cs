e0cbee9f2f7021e867bd957f916b97c35c4fdf91
Igor Rasskazov <writingornet@gmail.com>
2022-02-08 11:45:23 -0800

    Create Classes: Order, OrderItem, OrderRepository. Create Interface: IOrderRepository. Create tests: OrderTests, OrderItemTests.

---
+++ domain/Store/Order.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store
+{
+    public class Order
+    {
+        public int Id { get; }
+
+        private List<OrderItem> items;
+        public IReadOnlyCollection<OrderItem> Items
+        {
+            get { return items; }
+        }
+
+        public int TotalCount
+        {
+            get { return items.Sum(item => item.Count); }
+        }
+
+        public decimal TotalPrice
+        {
+            get { return items.Sum(item => item.Price * item.Count);  }
+        }
+
+        public Order(int id, IEnumerable<OrderItem> items)
+        {
+            if (items == null)
+                throw new ArgumentNullException(nameof(items));
+
+            Id = id;
+
+            this.items = new List<OrderItem>(items);
+        }
+    }
+}
---
+++ infrastructure/Store.Memory/OrderRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Memory
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly List<Order> orders = new List<Order>();
+
+        public Order Create()
+        {
+            int nextId = orders.Count + 1;
+            var order = new Order(nextId, new OrderItem[0]);
+
+            orders.Add(order);
+            return order;
+        }
+
+        public Order GetById(int id)
+        {
+            // если не будет найден заказ с таким id то Сингл выбросит исключение, как и если их будет больше одного
+            return orders.Single(order => order.Id == id);
+        }
+
+        public void Update(Order order)
+        {
+            ; // ничего не делает и работает правильно
+        }
+    }
+}
